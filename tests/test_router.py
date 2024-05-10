54d1669646b6714e41202751d49115a85ee4d0b1
Patrick Barker <patrickbarkerco@gmail.com>
2024-05-10 11:30:15 -0600

    more tests

--- tests/test_router.py
+++ tests/test_router.py
@@ -36,3 +36,23 @@ def test_router():
     assert prompt.response == response.msg
     assert prompt.response_schema is not None
     assert prompt.response_schema.model_json_schema() == Animal.model_json_schema()
+
+    thread = RoleThread()
+    thread.post(
+        role="user",
+        msg=("can you describe whats in this image? "),
+        images=[
+            "https://images.rawpixel.com/image_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvZnJwYW50aGVyYV90aWdyaXNfYWx0YWljYV90aWdlcl8wLWltYWdlLWt6eGx2YzYyLmpwZw.jpg"
+        ],
+    )
+
+    response = router.chat(thread)
+
+    print("\n\n!response: ", response)
+
+    prompts = Prompt.find(id=response.prompt_id)
+    assert len(prompts) == 1
+
+    prompt = prompts[0]
+    assert prompt.response == response.msg
+    assert prompt.response_schema is None
